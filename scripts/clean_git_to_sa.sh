8c50404d054761d3b1171027adf04ed96073e8c9
Pierre <pierre@depretz.eu>
2019-04-29 22:10:03 +0200

    scripts syn sa / github

---
+++ scripts/clean_git_to_sa.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+azSAKey="DefaultEndpointsProtocol=https;AccountName=cgghubdgconf01;AccountKey=zq/mRQumKZe/p5aUavFs23nvx/XdGhlXNT7KdH23yDEChN25d7PyodBDYGgp//ZmdjJztHrS3dWnBGucSgJnGQ==;EndpointSuffix=core.windows.net"
+ddShare="datadog"
+ddDir="TestPierre"
+
+function_purge_dir () {
+	checkFolder=`az storage directory exists -s $ddShare/$ddDir -n $1  --connection-string $azSAKey --output tsv`
+	if [ "$checkFolder" = "True" ]
+	then
+		for dir in `az storage file list -s $ddShare/$ddDir/$1 --connection-string $azSAKey --output json | jq '.[] | select (.type == "dir") | .name' -r`; do
+			delDir=`az storage directory delete -s $ddShare/$ddDir/$1 -n $dir --connection-string $azSAKey --output tsv`
+				if [ "$delDir" = "True" ]
+				then
+					echo "info: directory $1/$dir deleted"
+				else
+					echo "error: directory $1/$dir not deleted"
+				fi
+		done
+	else
+		echo "info: directory $1 doesn't exist"
+	fi
+}
+
+#YAML
+##DELETE FILES
+echo "info: clean all files"
+az storage file delete-batch -s $ddShare/$ddDir --pattern '*' --connection-string $azSAKey
+
+##FOLDER
+###AD
+function_purge_dir "conf.d/active_directory"
+###DEFAULT
+function_purge_dir "conf.d/default"
+###IIS
+function_purge_dir "conf.d/iis"
+###sep
+function_purge_dir "conf.d/sep"
+###sqlserver
+function_purge_dir "conf.d/sqlserver"
+###wsus
+function_purge_dir "conf.d/wsus"
+
+#CONF
+function_purge_dir "conf.d"
+
+#ROOT
+az storage directory delete -s $ddShare/$ddDir -n conf.d --connection-string $azSAKey --output none
+az storage directory delete -s $ddShare/$ddDir -n checks.d --connection-string $azSAKey --output none
--- scripts/refresh_git_to_sa.sh
+++ scripts/refresh_git_to_sa.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-#INITIALISATION
+#INITIALISATION VARIABLES
+#SA : cgghubdgconf01
 azSAKey="DefaultEndpointsProtocol=https;AccountName=cgghubdgconf01;AccountKey=zq/mRQumKZe/p5aUavFs23nvx/XdGhlXNT7KdH23yDEChN25d7PyodBDYGgp//ZmdjJztHrS3dWnBGucSgJnGQ==;EndpointSuffix=core.windows.net"
 ddShare="datadog"
 ddDir="TestPierre"
 tempDir="/tmp/tmpDirGit"
-gitRepo="https://github.com/deppierre/Datadog"
-checkFolder=`az storage directory exists -s $ddShare -n $ddDir --connection-string $azSAKey --output tsv`
+gitRepo="https://github.com/deppierre/datadog"
 
+#FUNCTION GIT CLONE
 function_github_sync () {
-	rm -rf $tempDir
 	git clone $gitRepo $tempDir
-	az storage file upload-batch -d $ddShare/$ddDir -s $tempDir --connection-string $azSAKey 
+
+	#COPIE DANS CONTAINER AZURE
+	az storage file upload-batch -d $ddShare/$ddDir -s $tempDir/agent_datadog --connection-string $azSAKey --output table
 }
+
+#MAIN
+#VERIFICATION DOSSIER DESTINATION
+checkFolder=`az storage directory exists -s $ddShare -n $ddDir --connection-string $azSAKey --output tsv`
+rm -rf $tempDir
 
 if [ "$checkFolder" = "False" ]
 then
@@ -21,4 +28,5 @@ then
 	function_github_sync
 else
 	echo "Temp folder :: $ddDir already exist"
+	function_github_sync
 fi
